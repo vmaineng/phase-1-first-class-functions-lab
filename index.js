3d346c877caaee683687e81148bac6313455c225
Mai Vang <vmaineng@gmail.com>
2022-04-27 09:18:36 -0700

    commit tree a2447dcfc84766c27271fb7c16897923ecffddb2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => array.slice(0,2);
+
+const returnLastTwoDrivers = (array) => array.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = integer => {
+    return fare => integer * fare
+};
+
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (array, returnFirstTwoDrivers) => returnFirstTwoDrivers(array);
